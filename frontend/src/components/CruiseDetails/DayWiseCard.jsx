60be05984629a4690d4fc1a0fc4d39d91e87cf55
Mahima Sachdeva <32546808+mahi397@users.noreply.github.com>
2024-12-03 18:45:11 -0500

    [Frontend] Cruise Details component family

---
+++ frontend/src/components/CruiseDetails/DayWiseCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function DayWiseCard() {
+  return (
+    <div>
+      <img></img>
+      <div>
+        <h2>Day {x}: {portname}</h2>
+        <h4>Departs at {departureTime}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
